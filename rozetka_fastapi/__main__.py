10b0b4ba2bd8805bb67772b13fff99295428ff33
Alexey ALERT Rubasheff <alexey.rubasheff@gmail.com>
2022-09-15 18:37:26 +0300

    iteration
    + caching
    + each field getters
    + all data getter

--- rozetka_fastapi/__main__.py
+++ rozetka_fastapi/__main__.py
@@ -1,12 +1,14 @@
 import os
+import json
+import pprint
 from collections import namedtuple
-
 from aiohttp_retry import ExponentialRetry, RetryClient
+from cashews import cache
 from fastapi import FastAPI, HTTPException
-from global_logger import Log
 from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
+from global_logger import Log
 
-log = Log.get_logger()
+LOG = Log.get_logger()
 
 app = FastAPI()
 
@@ -19,7 +21,7 @@ INFLUX_KWARGS_ASYNC = dict(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_
                            client_session_type=RetryClient,
                            timeout=600_000, client_session_kwargs={"retry_options": ExponentialRetry(attempts=3)})
 
-Record = namedtuple("Record", ('date', 'price'))
+Record = namedtuple("Record", ('date', 'value'))
 
 
 # @app.get("/")
@@ -27,20 +29,20 @@ Record = namedtuple("Record", ('date', 'price'))
 #     return {}
 
 
-@app.get("/get/{id_}")
-async def get_id(id_: int):
-    log.trace()
+@cache(ttl="12h")
+async def get_values(id_: int, field: str):
+    LOG.trace()
     async with InfluxDBClientAsync(**INFLUX_KWARGS_ASYNC) as client:
         ready = await client.ping()
         if not ready:
-            log.error(f"InfluxDB NOT READY")
+            LOG.error(f"InfluxDB NOT READY")
             raise HTTPException(status_code=502, detail="Database error. Please contact Telegram @ALERTua")
 
         query_api = client.query_api()
         query = f' from(bucket:"{INFLUXDB_BUCKET}") ' \
                 f' |> range(start: -100y)' \
                 f' |> filter(fn: (r) => r["_measurement"] == "{id_}")' \
-                f' |> filter(fn: (r) => r["_field"] == "price")' \
+                f' |> filter(fn: (r) => r["_field"] == "{field}")' \
                 f' |> aggregateWindow(every: 12h, fn: mean, createEmpty: false)'
         response = await query_api.query(query=query, org=INFLUXDB_ORG)
         # response = await query_api.query_raw(query=query, org=INFLUXDB_ORG)
@@ -51,4 +53,59 @@ async def get_id(id_: int):
         for record in records:
             record_ = Record(*record)
             output.append(record_)
-        return {i.date.timestamp() * 1000: i.price for i in output}
+        return {i.date.timestamp() * 1000: i.value for i in output}
+
+
+@cache(ttl="12h")
+async def get_data_by_id(id_: int):
+    LOG.trace()
+    async with InfluxDBClientAsync(**INFLUX_KWARGS_ASYNC) as client:
+        ready = await client.ping()
+        if not ready:
+            LOG.error(f"InfluxDB NOT READY")
+            raise HTTPException(status_code=502, detail="Database error. Please contact Telegram @ALERTua")
+
+        query_api = client.query_api()
+        query = f'''
+import "influxdata/influxdb/schema"
+from(bucket: "{INFLUXDB_BUCKET}")
+    |> range(start: -3y)
+    |> filter(fn: (r) => r["_measurement"] == "{id_}")
+    |> aggregateWindow(every: 12h, fn: mean, createEmpty: false)
+    |> sort(columns: ["_time"])
+    |> schema.fieldsAsCols()
+'''
+        response = await query_api.query(query=query, org=INFLUXDB_ORG)
+        if not response:
+            LOG.yellow(f"Empty response on {id_}")
+            output = []
+        else:
+            # response = await query_api.query_raw(query=query, org=INFLUXDB_ORG)
+            # json_ = response.to_json(["_stop", "discount", "old_price", "price"])
+            output = json.loads(response.to_json())
+            erase = ('result', 'table', '_measurement', '_start', '_stop')
+            for item in output:
+                for field in erase:
+                    item.pop(field, None)
+            LOG.green(f"{id_}: Returning\n{pprint.pformat(output)}")
+        return output
+
+
+@app.get("/price/{id_}")
+async def get_price(id_: int):
+    return await get_values(id_=id_, field='price')
+
+
+@app.get("/discount/{id_}")
+async def get_discount(id_: int):
+    return await get_values(id_=id_, field='discount')
+
+
+@app.get("/price_old/{id_}")
+async def get_price_old(id_: int):
+    return await get_values(id_=id_, field='old_price')
+
+
+@app.get("/get/{id_}")
+async def get_data(id_: int):
+    return await get_data_by_id(id_=id_)
