0c1582e6ae25c38aa046804ccf932580e067e5b7
Alexey ALERT Rubasheff <alexey.rubasheff@gmail.com>
2023-09-15 15:27:57 +0300

    + healthcheck

--- rozetka_fastapi/__main__.py
+++ rozetka_fastapi/__main__.py
@@ -5,9 +5,10 @@ import pprint
 from collections import namedtuple
 from aiohttp_retry import ExponentialRetry, RetryClient
 from cashews import cache
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
 from global_logger import Log
+from pydantic import BaseModel
 
 LOG = Log.get_logger()
 
@@ -110,6 +111,33 @@ async def get_price_old(id_: int):
 @app.get("/get/{id_}")
 async def get_data(id_: int):
     return await get_data_by_id(id_=id_)
+
+
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+
+    status: str = "OK"
+
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
 
 
 if __name__ == '__main__':
